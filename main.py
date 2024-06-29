74508a93dd245e96c2f43d7478534c78bb80c66c
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-06-29 16:02:34 -0500

    created GET all exercises route and successfull connection to MySQL database

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from flask import Flask, request, jsonify
+import pymysql.cursors
+
+def get_db_connection():
+    connection = pymysql.connect(
+        host='localhost',
+        password='cratose@41795',
+        user='root',
+        database='fitness-exercises',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
     return "Hello, Welcome to Fitness Tracker"
+
+@app.route("/exercises")
+def get_exercises():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM exercises;")
+        exercises = cursor.fetchall()
+        connection.close()
+        return jsonify(exercises), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 
 app.run(port=3005)
 
